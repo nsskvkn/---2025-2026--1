386edd372b35f00331b22da1aab3dbd6f2171f62
nsskvkn <d9127506@gmail.com>
2025-02-09 16:19:47 +0200

    вввв

--- Lab.css/Triangle1.h
+++ Lab.css/Triangle1.h
@@ -1,12 +1,11 @@
 #include "Triangle.h"
-#include <cmath> // для sqrt
+#include <cmath> /* для sqrt
+*/ 
 
-// Конструктор за замовчуванням
 Triangle::Triangle() {
     x1 = y1 = x2 = y2 = x3 = y3 = 0;
 }
 
-// Конструктор з параметрами
 Triangle::Triangle(double a, double b, double c, double d, double e, double f) {
     x1 = a;
     y1 = b;
@@ -16,7 +15,6 @@ Triangle::Triangle(double a, double b, double c, double d, double e, double f) {
     y3 = f;
 }
 
-// Функція для встановлення координат
 void Triangle::setCoordinates(double a, double b, double c, double d, double e, double f) {
     x1 = a;
     y1 = b;
@@ -26,7 +24,6 @@ void Triangle::setCoordinates(double a, double b, double c, double d, double e, 
     y3 = f;
 }
 
-// Обчислення периметра
 double Triangle::getPerimeter() {
     double sideA = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     double sideB = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
